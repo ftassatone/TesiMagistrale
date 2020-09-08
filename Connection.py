f01139dd49e19a4fb477eb04f5893113ed40a17e
Francesca <f.tassatone@studenti.unisa.it>
2020-09-08 18:08:57 +0200

    Creazione metodo find()

--- Connection.py
+++ Connection.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 from pprint import  pprint
+from bson import objectid
+import bson
+from bson.codec_options import CodecOptions
 
 #connessione in local a mongoDB
 client = MongoClient('localhost', 27017)
@@ -7,26 +10,16 @@ pprint("Connessione stabilita")
 
 #accesso al databaset e alla collezione interessata
 database = client["dataset_covid"]
-collection = database.covid_campania
-collection2 = database.covid19_italy_province
-listaCampania=[]
+collectionCampania = database.covid_campania
+collectionProvince = database.covid19_italy_province
 
-for documentCampania in collection.find():
-    listaCampania.append(documentCampania)
+for item in collectionCampania.find({}):
+    z = item["Latitudine"]
+    for itemProvince in collectionProvince.find():
+        if z == itemProvince["Latitude"]:
+            print("Latitudine: ", z, "Latitude: ", itemProvince["Latitude"])
 
-result = database.collection.aggregate([
-    {
-        '$lookup': {
-            'from': collection2,
-            'localField': 'Latitudine',
-            'foreignField': 'Latitude',
-            'as': 'x'
-        }
-    }
-])
 
-for item in result:
-    #print(item)
 
 
 
