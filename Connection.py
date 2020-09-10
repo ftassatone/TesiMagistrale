148af70a1e8218b9626609434220831c8d652137
Francesca <f.tassatone@studenti.unisa.it>
2020-09-10 19:02:47 +0200

    Implementato metodo per confrontare le regioni

--- Connection.py
+++ Connection.py
@@ -1,25 +1,39 @@
 from pymongo import MongoClient
-from pprint import  pprint
-from bson import objectid
-import bson
-from bson.codec_options import CodecOptions
+import csv
 
 #connessione in local a mongoDB
 client = MongoClient('localhost', 27017)
-pprint("Connessione stabilita")
+print("Connessione stabilita")
 
 #accesso al databaset e alla collezione interessata
-database = client["dataset_covid"]
-collectionCampania = database.covid_campania
-collectionProvince = database.covid19_italy_province
+db = client["dataset_covid"]
+collectionRegion = db["covid19_italy_region"]
+collectionProvince = db["covid19_italy_province"]
 
-for item in collectionCampania.find({}):
-    z = item["Latitudine"]
-    for itemProvince in collectionProvince.find():
-        if z == itemProvince["Latitude"]:
-            print("Latitudine: ", z, "Latitude: ", itemProvince["Latitude"])
+regionList = []
+regionIndex = []
+provinceList = []
+provinceIndex = []
+newline = ''
+
+with open('innovators.csv', 'w', ',') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
+
+for itemRegion in collectionRegion.find({}):
+   regionList.append(itemRegion)
+   regionIndex.append(itemRegion["RegionName"])
+
+for itemProvince in collectionRegion.find({}):
+   provinceList.append(itemProvince)
+   provinceIndex.append(itemProvince["RegionName"])
 
 
 
-
-
+for item in regionIndex:
+    for itemProvince in provinceIndex:
+        if item == itemProvince:
+            print(item, "-", itemProvince)
