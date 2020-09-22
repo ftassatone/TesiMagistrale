1679ed77283ef736a73f025349e8dbc5be7c718c
Francesca <f.tassatone@studenti.unisa.it>
2020-09-22 15:57:24 +0200

    Implementazione PCA

--- Connection.py
+++ Connection.py
@@ -1,57 +1,60 @@
-from pymongo import MongoClient
-import csv
-import numpy as np
+import sys
+import pandas as pd
+import pylab as pl
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
 
-#connessione in local a mongoDB
-#client = MongoClient('localhost', 27017)
-from pymongo.errors import BulkWriteError
+def main():
+	"""Load data."""
 
-client = MongoClient("mongodb+srv://admin:admin@tesifrancescatassatone.jlr3s.gcp.mongodb.net/dataset_covid?retryWrites=true&w=majority")
-print("Connessione stabilita")
+	csvfile = pd.read_csv("merge_regioni-province.csv")
+	data = pd.read_csv(csvfile, index_col=(0, 1), encoding='latin-1')
 
-#accesso al databaset e alla collezione interessata
-db = client["dataset_covid"]
-collectionRegion = db["covid19_italy_region"]
-collectionProvince = db["covid19_italy_province"]
+	# first column provides labels
+	ylabels = [a for a, _ in data.index]
+	labels = [text for _, text in data.index]
+	encoder = preprocessing.LabelEncoder().fit(ylabels)
 
-regionList = []
-regionIndex = []
-provinceList = []
-provinceIndex = []
-
-
-for itemRegion in collectionRegion.find({},{'_id': False}):
-   regionList.append(itemRegion)
-   regionIndex.append(itemRegion["RegionName"])
-
-for itemProvince in collectionProvince.find({},{'_id': False}):
-   provinceList.append(itemProvince)
-   provinceIndex.append(itemProvince["RegionName"])
-
-#se la collezione myOut esiste, la elimino e successivamente la ricreo
-myOut = db["myOut"]
-if "myOut" in db.list_collection_names():
-    myOut.drop()
-    print("Collection eliminata")
-db.create_collection("myOut")
-print("Collection creata")
-
-#implementazione match regioni
-arrReg = []
-arrProv = []
-
-for item in regionIndex:
-    flag = True
-    for item2 in provinceIndex:
-        if item == item2:
-            if flag == True:
-                reg = regionIndex.index(item)
-                arrReg.append(regionList[reg])
-                flag = False
-            prov = provinceIndex.index(item2)
-            arrProv.append(provinceList[prov])
+	xdata = data.as_matrix(data.columns)
+	ydata = encoder.transform(ylabels)
+	target_names = encoder.classes_
+	plotpca(xdata, ydata, target_names, labels, csvfile)
 
 
-for i in range(arrProv.__len__() ):
-    print("i ",i)
-    myOut.insert_one(provinceList[i])
+def plotpca(xdata, ydata, target_names, items, filename):
+	"""Make plot."""
+	pca = PCA(n_components=2)
+	components = pca.fit(xdata).transform(xdata)
+
+	# Percentage of variance explained for each components
+	print('explained variance ratio (first two components):',
+			pca.explained_variance_ratio_)
+
+	pl.figure()  # Make a plotting figure
+	pl.subplots_adjust(bottom=0.1)
+
+	# NB: a maximum of 7 targets will be plotted
+	for i, (c, m, target_name) in enumerate(zip(
+			'rbmkycg', 'o^s*v+x', target_names)):
+		pl.scatter(components[ydata == i, 0], components[ydata == i, 1],
+				color=c, marker=m, label=target_name)
+		for n, x, y in zip(
+				(ydata == i).nonzero()[0],
+				components[ydata == i, 0],
+				components[ydata == i, 1]):
+			pl.annotate(
+					items[n],
+					xy=(x, y),
+					xytext=(5, 5),
+					textcoords='offset points',
+					color=c,
+					fontsize='small',
+					ha='left',
+					va='top')
+	pl.legend()
+	pl.title('PCA of %s' % filename)
+	pl.show()
+
+
+if __name__ == '__main__':
+	main()
